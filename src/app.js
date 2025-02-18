e9f1cdb32a39a0ea25938e51e155c37ed17b8273
Carlos Melchor Perez <carloos0665@gmail.com>
2025-02-18 01:52:34 +0000

    nameGenerator actualizado

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@
   let pronouns = ['the', 'our','their'];
   let adjs = ['great', 'big' ];
   let nouns = ['jogger', 'racoon','apple','computer'];
-  
+  let extensiones = ['.com', '.net', '.io']
    
 
   for ( let pronoun of pronouns) {
     for ( let adj of adjs) {
       for ( let noun of nouns){
-        console.log( pronoun + " " + adj + " " + noun + ".com" )
+        for ( let extension of extensiones){
+          console.log( pronoun +  adj +  noun + extension  )
+        }
       }
     }
   }
