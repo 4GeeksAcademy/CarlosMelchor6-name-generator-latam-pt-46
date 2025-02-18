7a6b1d0780b44d2242418c1459f51c7f9f2d81fc
Carlos Melchor Perez <carloos0665@gmail.com>
2025-02-18 00:12:05 +0000

    add nameGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
-import "./style.css";
 
+  let pronouns = ['the', 'our'];
+  let adjs = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+  let extensions = ['.com', '.net'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+   
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for ( let pronoun of pronouns) {
+    for ( let adj of adjs) {
+      for ( let noun of nouns){
+        for (let extension of extensions) {
+        console.log( pronoun + " " + adj + " " + noun + extension)
+
+        }
+      }
+    }
+  }
